176e9bac41d3bf2890e56ff1fad60a267c702830
Ali Flores <mflores3@ncsu.edu>
2023-10-24 17:00:12 -0400

    added navigation to image gallery

---
+++ web-app/frontend/src/components/ImageGallery.jsx
@@ -0,0 +1,12 @@
+import React from "react"; 
+import SavedImages from './SavedImages';
+  
+const ImageGallery = () => { 
+  return ( 
+    <div> 
+      <SavedImages style={{ flexGrow: "0" }}/>
+    </div> 
+  ); 
+}; 
+  
+export default ImageGallery; 

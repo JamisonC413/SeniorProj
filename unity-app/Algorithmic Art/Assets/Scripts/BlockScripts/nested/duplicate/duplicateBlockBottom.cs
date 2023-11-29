754fd22ab0dd697d71b72b39dbd832c2a870f46d
jcox3 <jcox3@ncsu.edu>
2023-11-29 00:59:12 -0500

    Fixed nested duplication

--- unity-app/Algorithmic Art/Assets/Scripts/BlockScripts/nested/duplicate/duplicateBlockBottom.cs
+++ unity-app/Algorithmic Art/Assets/Scripts/BlockScripts/nested/duplicate/duplicateBlockBottom.cs
@@ -1,11 +1,93 @@
+using Codice.Client.Common.GameUI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class duplicateBlockBottom : nestedBottom
 { 
     public override void execute()
     {
-        ((duplicateBlock)topBlock).flag = false;
+        duplicateBlock dup = ((duplicateBlock)topBlock);
+
+        int angleSize = 360 / (dup.duplicate + 1);
+
+        //Track any new line/mesh renderers
+        int newLineLength = dup.brush.lineRenderers.Count;
+        int newMeshLength = dup.brush.meshRenderers.Count;
+
+        //For each new line renderer
+        for (int i = dup.oldLineListLength; i < newLineLength; i++)
+        {
+            if (newLineLength > dup.oldLineListLength)
+            {
+                //Make # of duplicates specified
+                for (int j = 0; j < dup.duplicate; j++)
+                {
+                    //Make a copy of the linerenderer 
+                    GameObject rotate = Instantiate(dup.brush.lineRenderers[i], new Vector3(0, 0, 0), Quaternion.identity);
+
+                    LineRenderer lineRenderer = rotate.GetComponent<LineRenderer>();
+                    Vector3[] positions = new Vector3[lineRenderer.positionCount];
+                    lineRenderer.GetPositions(positions);
+
+                    //For each position
+                    for (int k = 0; k < positions.Length; k++)
+                    {
+                        //Rotate the position around the starting brush point by the specified angle
+                        float radianNum = angleSize * (j + 1) / 180F * Mathf.PI;
+                        float sin = Mathf.Sin(radianNum);
+                        float cos = Mathf.Cos(radianNum);
+
+                        positions[k] -= new Vector3(dup.startPos.x, dup.startPos.y, 0);
+
+                        float xnew = positions[k].x * cos - positions[k].y * sin;
+                        float ynew = positions[k].x * sin + positions[k].y * cos;
+
+                        positions[k] = new Vector3(xnew + dup.startPos.x, ynew + dup.startPos.y, positions[k].z);
+                    }
+                    // Set the modified positions back to the LineRenderer
+                    lineRenderer.SetPositions(positions);
+
+                    Renderer rendererComponent = rotate.GetComponent<Renderer>();
+                    if (rendererComponent != null)
+                    {
+                        rendererComponent.sortingLayerName = "ImageRendering";
+                        rendererComponent.sortingOrder = 1;
+                    }
+
+                    //Add this new line renderer to the list and increase the list count
+                    dup.brush.lineRenderers.Add(rotate);
+                }
+
+            }
+        }
+
+        if (newMeshLength > dup.oldMeshListLength)
+        {
+            for (int i = dup.oldMeshListLength; i < newMeshLength; i++)
+            {
+
+                for (int j = 0; j < dup.duplicate; j++)
+                {
+                    GameObject rotate = Instantiate(dup.brush.meshRenderers[i], dup.brush.meshRenderers[i].transform.position, Quaternion.identity);
+                    //Make a copy of the meshrenderer and rotate it around the starting brush position
+                    rotate.GetComponent<MeshRenderer>().transform.RotateAround(dup.startPos, Vector3.forward, angleSize * (j + 1));
+
+                    //Add this new mesh renderer to the list
+                    Renderer rendererComponent = rotate.GetComponent<Renderer>();
+                    if (rendererComponent != null)
+                    {
+                        rendererComponent.sortingLayerName = "ImageRendering";
+                        rendererComponent.sortingOrder = 1;
+                    }
+                    dup.brush.meshRenderers.Add(rotate);
+                }
+               
+            }
+        }
     }
+
 }
--- unity-app/Algorithmic Art/Assets/Scripts/BlockScripts/nested/mirror/mirrorBlockBottom.cs
+++ unity-app/Algorithmic Art/Assets/Scripts/BlockScripts/nested/mirror/mirrorBlockBottom.cs
@@ -6,8 +6,119 @@ public class mirrorBlockBottom : nestedBottom
 { 
     public override void execute()
     {
-        ((mirrorBlock)topBlock).flag = false;
-        ((mirrorBlock)topBlock).mirror();
-        ((mirrorBlock)topBlock).brush.numMirrors--;
+        mirrorBlock mirror = ((mirrorBlock)topBlock);
+
+        //Track any new line/mesh renderers
+        int newLineLength = mirror.brush.lineRenderers.Count;
+        int newMeshLength = mirror.brush.meshRenderers.Count;
+
+        // Remake to use the createLineRenderer functions from Brush?
+
+        //For each new line renderer
+        for (int i = mirror.oldLineListLength; i < newLineLength; i++)
+        {
+            //Make a new line renderer to flip
+            GameObject flip = Instantiate(mirror.brush.lineRenderers[i], Vector3.zero, Quaternion.identity);
+
+            Vector2[] drawArea = mirror.brush.getDrawArea();
+            //Mirror across the correct axis
+            switch (mirror.axis)
+            {
+                //Across X axis
+                case 0:
+                    float xAxis = (Mathf.Abs((drawArea[0].y - drawArea[1].y)) / 2) + drawArea[0].y;
+
+                    // Multiply every Vector3 in positions by newScale
+                    LineRenderer lineRenderer = flip.GetComponent<LineRenderer>();
+                    Vector3[] positions = new Vector3[lineRenderer.positionCount];
+                    lineRenderer.GetPositions(positions);
+
+                    //Adjust each position to the mirrored value
+                    for (int j = 0; j < positions.Length; j++)
+                    {
+                        positions[j] = new Vector3(positions[j].x, positions[j].y + 2f * (xAxis - positions[j].y), positions[j].z);
+                    }
+
+                    // Set the modified positions back to the LineRenderer
+                    lineRenderer.SetPositions(positions);
+                    break;
+
+                //Across Y axis
+                case 1:
+
+                    float yAxis = (Mathf.Abs((drawArea[0].x - drawArea[1].x)) / 2) + drawArea[0].x;
+
+                    lineRenderer = flip.GetComponent<LineRenderer>();
+                    positions = new Vector3[lineRenderer.positionCount];
+                    lineRenderer.GetPositions(positions);
+
+                    for (int j = 0; j < positions.Length; j++)
+                    {
+                        positions[j] = new Vector3(positions[j].x + 2f * (yAxis - positions[j].x), positions[j].y, positions[j].z);
+                    }
+
+                    // Set the modified positions back to the LineRenderer
+                    lineRenderer.SetPositions(positions);
+                    break;
+
+                default: break;
+
+            }
+
+            Renderer rendererComponent = flip.GetComponent<Renderer>();
+            if (rendererComponent != null)
+            {
+                rendererComponent.sortingLayerName = "ImageRendering";
+                rendererComponent.sortingOrder = 1;
+            }
+
+            mirror.brush.lineRenderers.Add(flip);
+        }
+
+        //For each new mesh renderer
+        for (int i = mirror.oldMeshListLength; i < newMeshLength; i++)
+        {
+            //Make a new mesh renderer to flip
+            GameObject flip = Instantiate(mirror.brush.meshRenderers[i], mirror.brush.meshRenderers[i].transform.position, Quaternion.identity);
+
+            Vector2[] drawArea = mirror.brush.getDrawArea();
+            //Mirror across the correct axis
+            switch (mirror.axis)
+            {
+                //Across X axis
+                case 0:
+                    float xAxis = (Mathf.Abs((drawArea[0].y - drawArea[1].y)) / 2) + drawArea[0].y;
+
+                    flip.transform.localScale = new Vector3(flip.transform.localScale.x, -flip.transform.localScale.y, flip.transform.localScale.z);
+
+                    flip.transform.Translate(0f, (xAxis - flip.transform.position.y) * 2, 0f);
+                    //float newDistance = 5F - flip.transform.position.y;
+                    //flip.transform.position = new Vector3(flip.transform.position.x, newDistance - 1.92F, flip.transform.position.z);
+                    break;
+                //Across Y axis
+                case 1:
+
+                    float yAxis = (Mathf.Abs((drawArea[0].x - drawArea[1].x)) / 2) + drawArea[0].x;
+
+                    flip.transform.localScale = new Vector3(-flip.transform.localScale.x, flip.transform.localScale.y, flip.transform.localScale.z);
+
+                    flip.transform.Translate((yAxis - flip.transform.position.x) * 2, 0f, 0f);
+
+                    break;
+
+                default: break;
+
+            }
+
+            Renderer rendererComponent = flip.GetComponent<Renderer>();
+            if (rendererComponent != null)
+            {
+                rendererComponent.sortingLayerName = "ImageRendering";
+                rendererComponent.sortingOrder = 1;
+            }
+            mirror.brush.meshRenderers.Add(flip);
+
+        }
+
     }
 }
